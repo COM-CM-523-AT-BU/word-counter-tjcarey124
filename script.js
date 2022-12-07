fcd18c2ef51464d8199b6b2fae2bbf1f6fb77a81
tjcarey124 <113481777+tjcarey124@users.noreply.github.com>
2022-12-06 21:08:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-// put your globals here - hint: select elements from the HTML
+const textArea = document.querySelector("#text-area");
+const countBtn = document.querySelector("#countBtn");
+const wordCountInput = document.querySelector("#wordCountInput");
+const findWordInput = document.querySelector("#findWordInput");
+const findBtn = document.querySelector("#findBtn");
+const wordFoundInput = document.querySelector("#wordFoundInput");
 
-
-// dont' forget to add event listeners to teh buttons
+countBtn.addEventListener('click', countWords);
+findBtn.addEventListener('click', findWords);
 
 function countWords() {
-  // your code here
-  // call printData
+  const textContent = textArea.value;
+  const arr = textContent.split(" ");
+  printData(arr.length, wordCountInput);
 }
 
-function findWords() {
-  // put your local variable for the empty array here
-  // remainder of your code follows
-  // call printData
+function findWords(){
+  const foundWords = [];
+  const textContent = textArea.value;
+  const arr = textContent.split(" ");
+  arr. forEach( (item) => {
+    if (item === findWordInput.value) {
+      foundWords.push(item);
+    }
+  });
+  printData(foundWords.length, wordFoundInput);
 }
 
-// change param1 and param2 to identifiers that make sense
-function printData(param1, param2) {
-  // your code here... one line!
+function printData(data, destination){
+    destination.value = data 
 }
